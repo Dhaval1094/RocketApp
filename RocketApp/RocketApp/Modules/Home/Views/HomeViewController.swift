0e62a2bb0010e4092e5a8337d6fa7271480b84e8
Dhaval Trivedi <trivedidhavalpunit@gmail.com>
2021-06-19 14:18:34 +0530

    Setup UI for in Home storyboard

--- RocketApp/RocketApp/Modules/Home/Views/HomeViewController.swift
+++ RocketApp/RocketApp/Modules/Home/Views/HomeViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    //MARK: - IBOutlets
+    @IBOutlet weak var btnLoadMore: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var detailContainerView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
+    //MARK: - Variables
+    
     //MARK: - ViewController Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,4 +29,18 @@ class HomeViewController: UIViewController {
         title = NavigationTitle.HomeViewTitle
     }
 
+}
+
+//MARK: - UICollection View Delegate/Datasource
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
 }
